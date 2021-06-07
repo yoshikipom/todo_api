7c95cb9c95ea6903912dffa4019b09d4ccdc27ea
Yoshiki Shino <yoshiki.shino.tech@gmail.com>
2021-06-07 22:21:07 +0900

    feat: add model

---
+++ src/main/java/com/example/todo/domain/model/TodoUpdateCommand.java
@@ -0,0 +1,16 @@
+package com.example.todo.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoUpdateCommand {
+
+  private int id;
+  private String value;
+}
