52889bafda2727d6462fcbea5ca0c031e00c4608
Yoshiki Shino <yoshiki.shino.tech@gmail.com>
2021-06-12 01:32:00 +0900

    feat: implement TodoController

---
+++ src/main/java/com/example/todo/app/controller/todo/TodoResponse.java
@@ -0,0 +1,16 @@
+package com.example.todo.app.controller.todo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoResponse {
+
+  private int id;
+  private String value;
+}
